323780614126e1a70bf81552ed427e1edb6c723d
Валерия Липская <valery.lipskaya03@gmail.com>
2021-09-03 11:50:26 +0300

    контроллер журнала

---
+++ EDiary/Controllers/MarksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDiary.Controllers
+{
+    public class MarksController : Controller
+    {
+        public IActionResult Jurnal()
+        {
+            return View();
+        }
+    }
+}
