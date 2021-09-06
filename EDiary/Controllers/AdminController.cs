ee5836c94a8424485493460fb907100e2b4a04ab
Валерия Липская <valery.lipskaya03@gmail.com>
2021-09-06 11:28:45 +0300

    неработающая ХУЙНЯ

---
+++ EDiary/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDiary.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AddStudent()
+        {
+            return PartialView("~/Views/Admin/_addStudent.cshtml");
+        }
+    }
+}
