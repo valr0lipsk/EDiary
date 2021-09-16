1d4347b54a22b7e721432fb7a4a2e254c2703ba2
alexkuper <kuper2468@gmail.com>
2021-09-16 12:21:37 +0300

    ненавижу х2

--- EDiary/Controllers/MarksController.cs
+++ EDiary/Controllers/MarksController.cs
@@ -14,56 +14,53 @@ namespace EDiary.Controllers
     public class MarksController : Controller
     {
         EDContext context;
-        private UserManager<IdentityUser> userManager;
-        public MarksController(UserManager<IdentityUser> userManager, EDContext context) => (this.userManager, this.context) = (userManager, context);
+        public MarksController(EDContext context) => this.context = context;
 
         //представление журнала
-        //public IActionResult Jurnal()
-        //{
-        //    return View();
-        //}
+        public IActionResult Jurnal()
+        {
+            return View();
+        }
 
         [HttpGet]
         public IActionResult Jurnal(int id)
         {
             var subid = id;
-            //var teacherFullName = (from user in context.users
-            //                       join tr in context.teachers on user.idUser equals tr.teacherUser
-            //                       join subTaught in context.subjectTaughts on tr.teacherId equals subTaught.teacherId
-            //                       join st in context.subjects on subTaught.subjectId equals st.subjectId
-            //                       join gr in context.groups on subTaught.groupId equals gr.groupId
-            //                       join aspusers in context.Users on user.userId equals aspusers.Id
-            //                       where subTaught.tsubjectId == subid
-            //                       select new Users
-            //                       {
-            //                           userSurname = user.userSurname,
-            //                           userName = user.userName,
-            //                           userLastname = user.userLastname
-            //                       }).ToList();
-            //var subjects = (from user in context.users
-            //                join tr in context.teachers on user.idUser equals tr.teacherUser
-            //                join subTaught in context.subjectTaughts on tr.teacherId equals subTaught.teacherId
-            //                join st in context.subjects on subTaught.subjectId equals st.subjectId
-            //                join gr in context.groups on subTaught.groupId equals gr.groupId
-            //                join aspusers in context.Users on user.userId equals aspusers.Id
-            //                where subTaught.tsubjectId == subid
-            //                select new Subject
-            //                {
-            //                    subjectName = st.subjectName
-            //                }).ToList();
-            //var group = (from user in context.users
-            //             join tr in context.teachers on user.idUser equals tr.teacherUser
-            //             join subTaught in context.subjectTaughts on tr.teacherId equals subTaught.teacherId
-            //             join st in context.subjects on subTaught.subjectId equals st.subjectId
-            //             join gr in context.groups on subTaught.groupId equals gr.groupId
-            //             join aspusers in context.Users on user.userId equals aspusers.Id
-            //             where subTaught.tsubjectId == subid
-            //             select new collegeGroup
-            //             {
-            //                 groupName = gr.groupName,
-            //             }).ToList();
-            //var teacherGroup = new TeacherGroupSubjectModel { Groups = group, Subjects = subjects, Users = teacherFullName };
-            return View(/*teacherGroup*/);
+            var teacherFullName = (from user in context.users
+                                   join tr in context.teachers on user.idUser equals tr.teacherUser
+                                   join subTaught in context.subjectTaughts on tr.teacherId equals subTaught.teacherId
+                                   join aspusers in context.Users on user.userId equals aspusers.Id
+                                   where subTaught.tsubjectId == subid
+                                   select new Users
+                                   {
+                                       userSurname = user.userSurname,
+                                       userName = user.userName,
+                                       userLastname = user.userLastname
+                                   }).ToList();
+            var subjects = (from user in context.users
+                            join tr in context.teachers on user.idUser equals tr.teacherUser
+                            join subTaught in context.subjectTaughts on tr.teacherId equals subTaught.teacherId
+                            join st in context.subjects on subTaught.subjectId equals st.subjectId
+                            join gr in context.groups on subTaught.groupId equals gr.groupId
+                            join aspusers in context.Users on user.userId equals aspusers.Id
+                            where subTaught.tsubjectId == subid
+                            select new Subject
+                            {
+                                subjectName = st.subjectName
+                            }).ToList();
+            var group = (from user in context.users
+                         join tr in context.teachers on user.idUser equals tr.teacherUser
+                         join subTaught in context.subjectTaughts on tr.teacherId equals subTaught.teacherId
+                         join st in context.subjects on subTaught.subjectId equals st.subjectId
+                         join gr in context.groups on subTaught.groupId equals gr.groupId
+                         join aspusers in context.Users on user.userId equals aspusers.Id
+                         where subTaught.tsubjectId == subid
+                         select new collegeGroup
+                         {
+                             groupName = gr.groupName,
+                         }).ToList();
+            var teacherGroup = new TeacherGroupSubjectModel { Groups = group, Subjects = subjects, Users = teacherFullName };
+            return View(teacherGroup);
         }
     }
 }
