be3bc1854df2157ebe1cfb296effffadc5d53f34
Валерия Липская <valery.lipskaya03@gmail.com>
2021-09-27 11:35:13 +0300

    работает журнал

--- EDiary/Controllers/MarksController.cs
+++ EDiary/Controllers/MarksController.cs
@@ -47,64 +47,6 @@ namespace EDiary.Controllers
         public IActionResult Jurnal(int id)
         {
             var subid = id;
-            /*рабочий варик*/
-            //var teacherJurnal = (from user in context.users
-            //                    join tr in context.teachers on user.idUser equals tr.teacherUser
-            //                    join subTaught in context.subjectTaughts on tr.teacherId equals subTaught.teacherId
-            //                    join st in context.subjects on subTaught.subjectId equals st.subjectId
-            //                    join gr in context.groups on subTaught.groupId equals gr.groupId
-            //                    join aspusers in context.Users on user.userId equals aspusers.Id
-            //                    where subTaught.tsubjectId == subid
-            //                    select new TeacherJurnalModel
-            //                    {
-            //                        teacherSurname = user.userSurname,
-            //                        teacherName = user.userName,
-            //                        teacherLastname = user.userLastname,
-            //                        groups = gr.groupName,
-            //                        subjectName = st.subjectName
-            //                    }).ToList();
-            //var studentJurnal = (from user in context.users
-            //                     join st in context.students on user.idUser equals st.studentUser
-            //                     join setMark in context.setMarks on st.studentId equals setMark.studentId
-            //                     join mark in context.marks on setMark.markId equals mark.markId
-            //                     join lesson in context.lessons on setMark.lessonId equals lesson.lessonId
-            //                     join subTaught in context.subjectTaughts on lesson.tsubjectId equals subTaught.tsubjectId
-            //                     join gr in context.groups on st.studentGroup equals gr.groupId
-            //                     join aspusers in context.Users on user.userId equals aspusers.Id
-            //                     where lesson.tsubjectId == subid
-            //                     select new StudentsJurnalModel
-            //                     {
-            //                         studentSurname = user.userSurname,
-            //                         studentName = user.userName,
-            //                         studentLastname = user.userLastname,
-            //                     }).ToList();
-            //var lessonJurnal = (from user in context.users
-            //                    join st in context.students on user.idUser equals st.studentUser
-            //                    join setMark in context.setMarks on st.studentId equals setMark.studentId
-            //                    join mark in context.marks on setMark.markId equals mark.markId
-            //                    join lesson in context.lessons on setMark.lessonId equals lesson.lessonId
-            //                    join subTaught in context.subjectTaughts on lesson.tsubjectId equals subTaught.tsubjectId
-            //                    join gr in context.groups on st.studentGroup equals gr.groupId
-            //                    join aspusers in context.Users on user.userId equals aspusers.Id
-            //                    where lesson.tsubjectId == subid
-            //                    select new StudentsJurnalModel
-            //                    {
-            //                        lessonDate = lesson.lessonDate.ToString("dd.MM.yyyy")
-            //                    }).ToList();
-            //var markJurnal = (from user in context.users
-            //                  join st in context.students on user.idUser equals st.studentUser
-            //                  join setMark in context.setMarks on st.studentId equals setMark.studentId
-            //                  join mark in context.marks on setMark.markId equals mark.markId
-            //                  join lesson in context.lessons on setMark.lessonId equals lesson.lessonId
-            //                  join subTaught in context.subjectTaughts on lesson.tsubjectId equals subTaught.tsubjectId
-            //                  join gr in context.groups on st.studentGroup equals gr.groupId
-            //                  join aspusers in context.Users on user.userId equals aspusers.Id
-            //                  where lesson.tsubjectId == subid
-            //                  select new StudentsJurnalModel
-            //                  {
-            //                      studentMark = mark.mark
-            //                  }).ToList();
-
             /*преподаватель, группа и предмет*/
             var teacherJurnal = (from user in context.users
                                  join tr in context.teachers on user.idUser equals tr.teacherUser
@@ -116,6 +58,7 @@ namespace EDiary.Controllers
                                      userName = user.userName,
                                      userLastname = user.userLastname
                                  }).ToList();
+
             var groupJurnal = (from subTaught in context.subjectTaughts
                                join gr in context.groups on subTaught.groupId equals gr.groupId
                                where subTaught.tsubjectId == subid
@@ -136,39 +79,21 @@ namespace EDiary.Controllers
                                   join user in context.users on st.studentUser equals user.idUser
                                   select new Users
                                   {
+                                      idUser = st.studentId,
                                       userSurname = user.userSurname,
                                       userName = user.userName,
                                       userLastname = user.userLastname
                                   }).ToList();
-            var studentsId = (from setMark in context.setMarks
-                              join st in context.students on setMark.studentId equals st.studentId
-                              join user in context.users on st.studentUser equals user.idUser
-                              join lesson in context.lessons on setMark.lessonId equals lesson.lessonId
-                              join mark in context.marks on setMark.markId equals mark.markId
-                              join subTaught in context.subjectTaughts on lesson.tsubjectId equals subTaught.tsubjectId
-                              where subTaught.tsubjectId == subid
-                              select new Student
-                              {
-                                  studentId = st.studentId,
-                              }).ToList();
+
             var lessonJurnal = (from lesson in context.lessons
-                                join setMark in context.setMarks on lesson.lessonId equals setMark.lessonId
+                                //join setMark in context.setMarks on lesson.lessonId equals setMark.lessonId
                                 where lesson.tsubjectId == subid
                                 select new Lesson
                                 {
                                     lessonId = lesson.lessonId,
                                     lessonDate = lesson.lessonDate
                                 }).ToList();
-            var markJurnal = (from setMark in context.setMarks
-                              join mark in context.marks on setMark.markId equals mark.markId
-                              join st in context.students on setMark.studentId equals st.studentId
-                              join lesson in context.lessons on setMark.lessonId equals lesson.lessonId
-                              join subTaught in context.subjectTaughts on lesson.tsubjectId equals subTaught.tsubjectId
-                              where subTaught.tsubjectId == subid
-                              select new Mark
-                              {
-                                  mark = mark.mark
-                              }).ToList();
+
             var setMarks = (from setMark in context.setMarks
                             join st in context.students on setMark.studentId equals st.studentId
                             join lesson in context.lessons on setMark.lessonId equals lesson.lessonId
@@ -177,11 +102,14 @@ namespace EDiary.Controllers
                             where subTaught.tsubjectId == subid
                             select new setMark
                             {
+                                mark = new Mark() { mark = mark.mark, markId = mark.markId},
+                                lessonId = setMark.lessonId,
                                 setmarkId = setMark.setmarkId,
                                 studentId = setMark.studentId
                             }).ToList();
+
             //var studentJurnal = new StudentsJurnalModel { Users = studentsJurnal, Marks = markJurnal, Lessons = lessonJurnal };
-            var jurnal = new JurnalModel { Teachers = teacherJurnal, Groups = groupJurnal, Lessons = lessonJurnal, Marks = markJurnal, Students = studentsJurnal, Subjects = subjectJurnal, StudentsId = studentsId, setMarks = setMarks };
+            var jurnal = new JurnalModel { Teachers = teacherJurnal, Groups = groupJurnal, Lessons = lessonJurnal, Students = studentsJurnal, Subjects = subjectJurnal, setMarks = setMarks };
             return View(jurnal);
         }
     }
