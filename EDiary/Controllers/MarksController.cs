69ab695bb32d8675bc8ddc53ab03ee3a85de622c
alexkuper <kuper2468@gmail.com>
2022-01-10 16:30:44 +0300

    обнова

--- EDiary/Controllers/MarksController.cs
+++ EDiary/Controllers/MarksController.cs
@@ -91,5 +91,14 @@ namespace EDiary.Controllers
             var jurnal = new JurnalModel { Teachers = teacherJurnal, Groups = groupJurnal, Lessons = lessonJurnal, Students = studentsJurnal, Subjects = subjectJurnal, setMarks = setMarks };
             return View(jurnal);
         }
+        public void updateMark(int id, string value)
+        {
+            var markId = value;
+            var setmark = id;
+            var updatedMark = context.setMarks.Where(sM => sM.setmarkId == id).First();
+            updatedMark.markId = (from mark in context.marks where mark.mark == value select mark.markId).First();
+            context.setMarks.Update(updatedMark);
+            context.SaveChanges();
+        }
     }
 }
