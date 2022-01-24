e178ec391623657322383d81a4f1016bc9010579
alexkuper <kuper2468@gmail.com>
2022-01-24 13:16:39 +0300

    скоро закончим журнал!!!

--- EDiary/ViewModels/JurnalModel.cs
+++ EDiary/ViewModels/JurnalModel.cs
@@ -11,12 +11,12 @@ namespace EDiary.ViewModels
         public IEnumerable<Teacher> Teachers { get; set; }
         public IEnumerable<Lesson> Lessons { get; set; }
         public IEnumerable<collegeGroup> Groups { get; set; }
-        public IEnumerable<Subject> Subjects { get; set; }
+        public IEnumerable<SubjectGroupModel> Subjects { get; set; }
         public IEnumerable<Student> Students { get; set; }
         public IEnumerable<setMark> setMarks { get; set; }
         public IEnumerable<lessonType> types { get; set; }
         public IEnumerable<Labs> labs { get; set; }
-        public IEnumerable<SubjectGroupModel> teacherSubjects { get; set; }
+        public IEnumerable<SubjectGroupModel> userSubjects { get; set; }
         public LessonModel addLesson { get; set; }
         public LessonModel deleteLesson { get; set; }
     }
