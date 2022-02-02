77cf72b91bf7c7142c25cca1c754026e63e03ded
valr0lipsk <valery.lipskaya03@gmail.com>
2022-02-02 20:30:03 +0300

    инитиал коммит

---
+++ EDiary/wwwroot/js/currentDateTimeModule.js
@@ -0,0 +1,20 @@
+﻿function zero_first_format(value) {
+    if (value < 10) {
+        value = '0' + value;
+    }
+    return value;
+}
+
+function date_time() {
+    let current_datetime = new Date();
+    let day = zero_first_format(current_datetime.getDate());
+    let month = zero_first_format(current_datetime.getMonth() + 1);
+    let year = current_datetime.getFullYear();
+    let hours = zero_first_format(current_datetime.getHours());
+    let minutes = zero_first_format(current_datetime.getMinutes());
+    let seconds = zero_first_format(current_datetime.getSeconds());
+
+    return hours + ":" + minutes + ":" + seconds + " " + day + "." + month + "." + year;
+};
+
+export { date_time }
